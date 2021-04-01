0f6e6c2405a7d7d9fdcfe2f63d467a044b269774
Richard Ho <skynet.tw@gmail.com>
2021-04-01 08:57:10 +0800

    fix

--- datacenter/settings.py
+++ datacenter/settings.py
@@ -13,7 +13,6 @@ import os
 import django_heroku
 from pathlib import Path
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
